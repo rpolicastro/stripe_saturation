cd73ff161acb5120faa9fec4d141aa5206f7bff2
rpolicastro <rpolicas@indiana.edu>
2019-07-25 11:46:14 -0400

    working version of saturation analysis

---
+++ R/stripe_saturation.R
@@ -0,0 +1,162 @@
+#!/usr/bin/env Rscript
+
+library("tidyverse")
+library("getopt")
+library("ChIPseeker")
+library("GenomicFeatures")
+library("GenomicRanges")
+library("TSRchitect")
+
+##########################################
+## Saturation Analysis of STRIPE-seq Data
+##########################################
+
+## Make Sample Sheet
+## ----------
+
+## Get sample files.
+
+sample.files <- list.files(file.path(opt$outdir, "sampled_bams"), pattern=".*\\.bam$")
+
+## Get sample names.
+
+sample.names <- str_replace(sample.files, "_Aligned.out.bam", "")
+
+## Make sample sheet.
+
+samples <- tibble(
+	"SAMPLE"=sample.names,
+	"ReplicateID"=1,
+	"FILE"=file.path(opt$outdir, "sampled_bams", sample.files)
+)
+
+## Export sample sheet.
+
+write.table(
+	samples, file.path(opt$outdir, "sampled_bams", "samples.tsv"),
+	sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE
+)
+
+## Find TSSs
+## ----------
+
+## Load TSS object.
+
+tss.obj <- loadTSSobj(
+	experimentTitle="STRIPE-seq Saturation Analysis",
+	inputDir=file.path(opt$outdir, "sampled_bams"),
+	n.cores=as.numeric(opt$cores),
+	isPairedBAM=as.logical(opt$paired),
+	isPairedBED=FALSE,
+	sampleSheet="samples.tsv",
+	sampleNames="",
+	replicateIDs=""
+)
+
+## Get fragment 5` ends.
+
+tss.obj <- inputToTSS(tss.obj)
+
+## Find TSSs.
+
+tss.obj <- processTSS(
+	tss.obj,
+	n.cores=opt$cores,
+	tssSet="all",
+	writeTable=FALSE
+)
+
+## Grab TSSs from object.
+
+TSSs <- tss.obj@tssCountData %>%
+	setNames(tss.obj@sampleNames)
+
+## Convert TSSs to GRanges object.
+
+TSSs.granges <- map(
+	TSSs,
+	~ GRanges(
+		seqnames=.$seq,
+		ranges=IRanges(start=.$TSS, width=1),
+		strand=.$strand,
+		score=.$nTAGs
+	)
+)
+
+## Export TSS Granges.
+
+dir.create(file.path(opt$outdir, "Rdata"))
+
+saveRDS(TSSs.granges, file.path(opt$outdir, "Rdata", "TSSs.RDS"))
+
+## Annotate TSSs
+## ----------
+
+## Load genome annotation.
+
+annotation <- makeTxDbFromGFF(opt$gtf, format="gtf")
+
+## Annotate TSSs.
+
+TSSs.annotated <- map(
+	TSSs.granges,
+	~ annotatePeak(.,
+		tssRegion=c(-1000,100),
+		TxDb=annotation,
+		sameStrand=TRUE
+	) %>%
+	as_tibble
+)
+
+## Saturation Analysis
+## ----------
+
+## Saturation analysis function.
+
+saturation.analysis <- function(x) {
+	n.genes <- x %>%
+		filter(score >= 3) %>%
+		pull(geneId) %>%
+		unique %>%
+		length
+
+	promoter.proximal.frac <- x %>%
+		filter(score >= 3) %>%
+		count(annotation) %>%
+		mutate(frac=n/sum(n)) %>%
+		filter(annotation == "Promoter") %>%
+		pull(frac)
+
+	return(tibble(n.genes, promoter.proximal.frac))
+}
+
+## Get saturation.
+
+TSS.saturation <- map(TSSs.annotated, ~saturation.analysis(.))
+
+## Plot Saturation
+## ----------
+
+## Create outdir directory.
+
+dir.create(file.path(opt$outdir, "saturation_analysis_plots"))
+
+## Format data for plotting.
+
+plotting.data <- TSS.saturation %>%
+	bind_rows(.id="sample") %>%
+	mutate(sample_value=str_remove(sample, pattern="_sampled.+")) %>%
+	mutate(sample_value=as.numeric(sample_value)) %>%
+	arrange(sample_value)
+
+## Plot data.
+
+p <- ggplot(plotting.data, aes(x=sample_value, y=n.genes)) +
+	geom_point(aes(size=promoter.proximal.frac, color=promoter.proximal.frac)) +
+	geom_line() +
+	scale_color_viridis_c()
+
+ggsave(
+	file.path(opt$outdir, "saturation_analysis_plots", "saturation_plot.pdf"),
+	plot=p, device="pdf", height=5, width=7
+)
--- main.sh
+++ main.sh
@@ -18,6 +18,31 @@ READS=$(samtools flagstat $BAM | awk 'NR==1' | cut -d" " -f1)
 
 ## Divide number by 2 if paired.
 
-[[ $PAIRED = TRUE ]] && READS=$(python -c "print $READS/2")
+if [ $PAIRED = TRUE ]; then 
+	READS=$(python -c "print(${READS}/2)")
+fi
 
+## Get sampling percentages.
 
+SAMPLES=($(seq $FROM $BY $TO))
+
+SAMPLE_FRACS=()
+for SAMPLE in ${SAMPLES[@]}; do
+	SAMPLE_FRACS+=($(python -c "print(float($SAMPLE)/float($READS))"))
+done
+
+## Sample BAMs and remove PCR duplicates.
+
+mkdir -p ${OUTDIR}/sampled_bams
+
+N_SAMPLES=${#SAMPLE_FRACS[@]}
+for N in $(seq 0 1 $(bc <<< $N_SAMPLES-1)); do
+	samtools view -bs 0${SAMPLE_FRACS[$N]} $BAM | \
+	samtools sort -n -@ $CORES - | \
+	samtools fixmate -m - - | \
+	samtools sort -@ $CORES - | \
+	samtools markdup - - | \
+	samtools view -F 3852 -f 3 -O BAM -@ $CORES -o ${OUTDIR}/sampled_bams/${SAMPLES[$N]}_sampled_$(basename $BAM)
+
+	samtools index ${OUTDIR}/sampled_bams/${SAMPLES[$N]}_sampled_$(basename $BAM)
+done
